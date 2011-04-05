97729d8316614c899200f4d04c9831ab6788cbd5
Joost Diepenmaat <joost@zeekat.nl>
2011-04-05 18:21:05 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pretzel "0.2.1"
+(defproject pretzel "0.2.2"
   :description "Pretzel - clojure predicate functions - strings and combinations"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
